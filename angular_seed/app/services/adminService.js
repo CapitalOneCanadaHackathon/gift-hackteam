d847c6950d0607f146b9200f61b8698ffeea5674
Rebecca Song <rebecca.song@outlook.com>
2016-10-22 15:38:21 -0400

    Add functionality to admin service

--- angular_seed/app/services/adminService.js
+++ angular_seed/app/services/adminService.js
@@ -8,55 +8,190 @@
 
         var adminService = {};
 
-        adminService.monthlyEvents = [];
+        adminService.keyCode = { 
+            "date": new Date("2016/10/22"),
+            "key": "AfL24Jfm1GKLylme234Po63M"  
+        };
 
-        
+        adminService.editUsersList = [];
+        adminService.users = [
+            {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
+            },
+                   {
+            "firstname": "Person",
+            "lastname": "Five",
+            "email": "personfive@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Six",
+            "email": "personsix@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Seven",
+            "email": "personseven@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Eight",
+            "email": "personeight@email.com",
+            "isAdmin": true,
+            },
+              {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
+            },
+                   {
+            "firstname": "Person",
+            "lastname": "Five",
+            "email": "personfive@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Six",
+            "email": "personsix@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Seven",
+            "email": "personseven@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Eight",
+            "email": "personeight@email.com",
+            "isAdmin": true,
+            },
+              {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
+            }
+        ];
+        adminService.numAdmin = 0;
+        adminService.numVolunteers = 0;
+        adminService.totalUsers = 0;
 
-        
-        //retrieve all events
-        eventService.getMonthlyEvents = function() {
-            ApiService.getMonthlyBookings()
-                .then(function(data){
-                    //must remove old bookings before trying to add new ones
-                    var numEvents = eventService.monthlyEvents.length;
-		            eventService.monthlyEvents.splice(0,numEvents);
-				    for(var i = 0; i<data.length; i++){//add events to array one by one
-                        var colour = eventService.pickEventColour(data[i].type);
-                        data[i].color = colour;
-                        eventService.monthlyEvents.push(data[i]);
+        adminService.generateKey = function() {
+            
+            var msPerDay = (24 * 60 * 60 * 1000);
+
+            console.log("Old key generated at: " + adminService.keyCode.date);
+            var currDate = new Date();
+            console.log("Current Date : " + currDate);
+            var dateDiff = currDate - adminService.keyCode.date;
+            console.log("Milliseconds since last generated key: " + dateDiff);
+
+            var daysPassed = Math.ceil(dateDiff / msPerDay);
+            console.log("Days since last generated key: " + daysPassed);
+
+            if (daysPassed > 1) {
+                adminService.keyCode.date = currDate;
+                adminService.keyCode.key = (Math.random() + 1).toString(36).substr(2, 24);
+            }
+
+        };
+
+        adminService.saveUsers = function() {
+
+            if (!adminService.equals(adminService.users, adminService.editUsersList)) {
+                // send to db
+                console.log("Saved Edited Users");
+                angular.copy(adminService.editUsersList, adminService.users);
+            }
+
+        };
+
+        adminService.equals = function(origUsers, editedUsers) {
+
+            var isEqual = true;
+
+            if (origUsers.length != editedUsers.length) {
+                isEqual = false;
+            } else {
+                for (var i = 0; i < origUsers.length; i++) {
+                    if (origUsers[i].isAdmin != editedUsers[i].isAdmin) {
+                        isEqual = false;
+                        break;
                     }
-                    console.log(eventService.monthlyEvents);
-                },
-                function(err){
-                    alert("error with getAllGroups");
-                });
-	    }
+                }
+            }
 
-        eventService.pickEventColour = function(type) {
-            if(type == "volunteer-meeting"){
-                return '#6371C6';
-            }
-            else if (type == "social"){
-                return '#37a3fb';
-            }
-            else if (type == "lunch"){
-                return '#2ca09e';
-            }
-            else if (type == "board-meeting"){
-                return '#990D6F';
-            }
-            else if (type == "family-meeting"){
-                return '#5931b4';
-            }
-            else if (type == "board-meeting"){
-                return '#5931b4';
-            }
-            else {
-                return '#2EB671';
-            }
-        }
+            return isEqual;
+        };
 
+        adminService.setUsers = function() {
+            angular.copy(adminService.users, adminService.editUsersList);
+        };
 
-        return eventService;
+        return adminService;
     }
 })();
--- angular_seed/app/view/admin/admin.controller.js
+++ angular_seed/app/view/admin/admin.controller.js
@@ -2,163 +2,173 @@
     'use strict';
     angular.module('faver.admin').controller('AdminController', AdminController);
 
-    function AdminController(){
+    function AdminController(AdminService){
         var vm = this;
         vm.test = "ADMINNNN";
         
         // copy original array of users into new array to detect changes made
 
-        vm.keyCode = { 
-            "date": new Date("2016/10/22"),
-            "key": "AfL24Jfm1GKLylme234Po63M"  
-        };
+        // vm.keyCode = { 
+        //     "date": new Date("2016/10/22"),
+        //     "key": "AfL24Jfm1GKLylme234Po63M"  
+        // };
 
-        vm.users = [
+        vm.keyCode = AdminService.keyCode;
 
-            {
-            "firstname": "Person",
-            "lastname": "One",
-            "email": "personone@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Two",
-            "email": "persontwo@email.com",
-            "isAdmin": true,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Three",
-            "email": "personthree@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Four",
-            "email": "personfour@email.com",
-            "isAdmin": true,
-            },
-                   {
-            "firstname": "Person",
-            "lastname": "Five",
-            "email": "personfive@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Six",
-            "email": "personsix@email.com",
-            "isAdmin": true,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Seven",
-            "email": "personseven@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Eight",
-            "email": "personeight@email.com",
-            "isAdmin": true,
-            },
-              {
-            "firstname": "Person",
-            "lastname": "One",
-            "email": "personone@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Two",
-            "email": "persontwo@email.com",
-            "isAdmin": true,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Three",
-            "email": "personthree@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Four",
-            "email": "personfour@email.com",
-            "isAdmin": true,
-            },
-                   {
-            "firstname": "Person",
-            "lastname": "Five",
-            "email": "personfive@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Six",
-            "email": "personsix@email.com",
-            "isAdmin": true,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Seven",
-            "email": "personseven@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Eight",
-            "email": "personeight@email.com",
-            "isAdmin": true,
-            },
-              {
-            "firstname": "Person",
-            "lastname": "One",
-            "email": "personone@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Two",
-            "email": "persontwo@email.com",
-            "isAdmin": true,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Three",
-            "email": "personthree@email.com",
-            "isAdmin": false,
-            },
-            {
-            "firstname": "Person",
-            "lastname": "Four",
-            "email": "personfour@email.com",
-            "isAdmin": true,
-            }
-        ];
+        // vm.users = AdminService.users;
+        AdminService.setUsers(); 
+        vm.editUsersList = AdminService.editUsersList;
+        // [
 
-        vm.numAdmin = 0;
-        vm.numVolunteers = 0;
-        vm.totalUsers = 0;
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "One",
+        //     "email": "personone@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Two",
+        //     "email": "persontwo@email.com",
+        //     "isAdmin": true,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Three",
+        //     "email": "personthree@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Four",
+        //     "email": "personfour@email.com",
+        //     "isAdmin": true,
+        //     },
+        //            {
+        //     "firstname": "Person",
+        //     "lastname": "Five",
+        //     "email": "personfive@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Six",
+        //     "email": "personsix@email.com",
+        //     "isAdmin": true,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Seven",
+        //     "email": "personseven@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Eight",
+        //     "email": "personeight@email.com",
+        //     "isAdmin": true,
+        //     },
+        //       {
+        //     "firstname": "Person",
+        //     "lastname": "One",
+        //     "email": "personone@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Two",
+        //     "email": "persontwo@email.com",
+        //     "isAdmin": true,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Three",
+        //     "email": "personthree@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Four",
+        //     "email": "personfour@email.com",
+        //     "isAdmin": true,
+        //     },
+        //            {
+        //     "firstname": "Person",
+        //     "lastname": "Five",
+        //     "email": "personfive@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Six",
+        //     "email": "personsix@email.com",
+        //     "isAdmin": true,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Seven",
+        //     "email": "personseven@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Eight",
+        //     "email": "personeight@email.com",
+        //     "isAdmin": true,
+        //     },
+        //       {
+        //     "firstname": "Person",
+        //     "lastname": "One",
+        //     "email": "personone@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Two",
+        //     "email": "persontwo@email.com",
+        //     "isAdmin": true,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Three",
+        //     "email": "personthree@email.com",
+        //     "isAdmin": false,
+        //     },
+        //     {
+        //     "firstname": "Person",
+        //     "lastname": "Four",
+        //     "email": "personfour@email.com",
+        //     "isAdmin": true,
+        //     }
+        // ];
 
-        vm.generateKey = function() {
+        vm.numAdmin = AdminService.numAdmin;
+        vm.numVolunteers = AdminService.numVolunteers;
+        vm.totalUsers = AdminService.totalUsers;
 
-            var msPerDay = (24 * 60 * 60 * 1000);
+        // vm.numAdmin = 0;
+        // vm.numVolunteers = 0;
+        // vm.totalUsers = 0;
 
-            console.log("Old key generated at: " + vm.keyCode.date);
-            var currDate = new Date();
-            console.log("Current Date : " + currDate);
-            var dateDiff = currDate - vm.keyCode.date;
-            console.log("Milliseconds since last generated key: " + dateDiff);
 
-            var daysPassed = Math.ceil(dateDiff / msPerDay);
-            console.log("Days since last generated key: " + daysPassed);
+        // vm.generateKey = function() {
 
-            if (daysPassed > 1) {
-                vm.keyCode.date = currDate;
-                vm.keyCode.key = (Math.random() + 1).toString(36).substr(2, 24);
-            }
-        };
+        //     var msPerDay = (24 * 60 * 60 * 1000);
+
+        //     console.log("Old key generated at: " + vm.keyCode.date);
+        //     var currDate = new Date();
+        //     console.log("Current Date : " + currDate);
+        //     var dateDiff = currDate - vm.keyCode.date;
+        //     console.log("Milliseconds since last generated key: " + dateDiff);
+
+        //     var daysPassed = Math.ceil(dateDiff / msPerDay);
+        //     console.log("Days since last generated key: " + daysPassed);
+
+        //     if (daysPassed > 1) {
+        //         vm.keyCode.date = currDate;
+        //         vm.keyCode.key = (Math.random() + 1).toString(36).substr(2, 24);
+        //     }
+        // };
 
         vm.countUsers = function() {
 
@@ -179,31 +189,35 @@
         };
 
         vm.saveUsers = function() {
-
-            if (!vm.equals(vm.users, vm.editUsersList)) {
-                // send to db
-                console.log("Saved Edited Users");
-                angular.copy(vm.editUsersList, vm.users);
-            }
+            AdminService.saveUsers();
         };
 
-        vm.equals = function(origUsers, editedUsers) {
+        // vm.saveUsers = function() {
 
-            var isEqual = true;
+        //     if (!vm.equals(vm.users, vm.editUsersList)) {
+        //         // send to db
+        //         console.log("Saved Edited Users");
+        //         angular.copy(vm.editUsersList, vm.users);
+        //     }
+        // };
 
-            if (origUsers.length != editedUsers.length) {
-                isEqual = false;
-            } else {
-                for (var i = 0; i < origUsers.length; i++) {
-                    if (origUsers[i].isAdmin != editedUsers[i].isAdmin) {
-                        isEqual = false;
-                        break;
-                    }
-                }
-            }
+        // vm.equals = function(origUsers, editedUsers) {
 
-            return isEqual;
-        };
+        //     var isEqual = true;
+
+        //     if (origUsers.length != editedUsers.length) {
+        //         isEqual = false;
+        //     } else {
+        //         for (var i = 0; i < origUsers.length; i++) {
+        //             if (origUsers[i].isAdmin != editedUsers[i].isAdmin) {
+        //                 isEqual = false;
+        //                 break;
+        //             }
+        //         }
+        //     }
+
+        //     return isEqual;
+        // };
 
         vm.removeUser = function(user) {
 
@@ -212,10 +226,10 @@
 
         };
 
-        vm.editUsersList = [];
-        angular.copy(vm.users, vm.editUsersList);
-
-        vm.generateKey();
+        // vm.editUsersList = [];
+        // angular.copy(AdminService.users, vm.editUsersList);
+        AdminService.generateKey();
+        // vm.generateKey();
         vm.countUsers();
 
     }
