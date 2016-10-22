dc50a63a3736b4fcfc6749008baa34c5128e22d1
Rebecca Song <rebecca.song@outlook.com>
2016-10-22 11:21:43 -0400

    Add basic view of admin page

--- angular_seed/app/view/admin/admin.controller.js
+++ angular_seed/app/view/admin/admin.controller.js
@@ -7,6 +7,8 @@
         vm.test = "ADMINNNN";
         //this should be displayed
         
+        vm.keyCode = "AfL24Jfm$^&4";
+
         vm.users = [
 
             {
@@ -32,9 +34,128 @@
             "lastname": "Four",
             "email": "personfour@email.com",
             "isAdmin": true,
+            },
+                   {
+            "firstname": "Person",
+            "lastname": "Five",
+            "email": "personfive@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Six",
+            "email": "personsix@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Seven",
+            "email": "personseven@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Eight",
+            "email": "personeight@email.com",
+            "isAdmin": true,
+            },
+              {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
+            },
+                   {
+            "firstname": "Person",
+            "lastname": "Five",
+            "email": "personfive@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Six",
+            "email": "personsix@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Seven",
+            "email": "personseven@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Eight",
+            "email": "personeight@email.com",
+            "isAdmin": true,
+            },
+              {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
             }
         ];
 
+        vm.numAdmin = 0;
+        vm.numVolunteers = 0;
+        vm.totalUsers = 0;
+
+        vm.countUsers = function() {
+
+            vm.numAdmin = 0;
+            vm.numVolunteers = 0;
+            vm.totalUsers = 0;
+
+            for (var i = 0; i < vm.users.length; i++) {
+                var user = vm.users[i];
+                if (user.isAdmin) {
+                    vm.numAdmin++;
+                } else {
+                    vm.numVolunteers++;
+                }
+                vm.totalUsers++;
+            }
+
+        };
+
+        vm.countUsers();
 
     }
 })();
