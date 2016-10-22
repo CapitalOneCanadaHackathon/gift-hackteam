c5c7ffaf14eaef602408b998d6a7d6a40bafa925
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-10-22 10:12:35 -0400

    api service creation

---
+++ angular_seed/app/services/apiService.js
@@ -0,0 +1,15 @@
+(function (){
+    'use strict';
+    angular.module('faver.core').service('ApiService', ApiService);
+
+    ApiService.$inject = [];
+
+    function ApiService(){
+
+        var apiService = {};
+
+        
+
+        return apiService;
+    }
+})();
