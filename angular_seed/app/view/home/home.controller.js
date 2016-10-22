91bd6ec467021de12ff29b33fc45c82d4f5b2f0e
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-10-22 09:45:31 -0400

    basic event display on calendar

--- angular_seed/app/view/home/home.controller.js
+++ angular_seed/app/view/home/home.controller.js
@@ -2,11 +2,47 @@
     'use strict';
     angular.module('faver.home').controller('HomeController', HomeController);
 
-    function HomeController(){
+    HomeController.$inject = ['$scope','uiCalendarConfig'];
+
+    function HomeController($scope,uiCalendarConfig){
         var vm = this;
-        vm.test = "TTTTEST";
+
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
         //this should be displayed
-        
-        vm.eventSources = [];
+
+        $scope.events = [
+            {title: 'All Day Event',start: new Date(y, m, 1)},
+            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+            {title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+            {title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+            {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+            {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29)}
+        ];
+
+        $scope.eventSources = [$scope.events];
+
+        $scope.alertOnEventClick = function( date, jsEvent, view){
+            console.log("clicked");
+        };
+
+        /* config object */
+        $scope.uiConfig = {
+            calendar:{
+                height: 450,
+                editable: true,
+                header:{
+                left: 'title',
+                center: '',
+                right: 'today prev,next'
+                },
+                eventClick: $scope.alertOnEventClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize
+            }
+        };
+
     }
 })();
