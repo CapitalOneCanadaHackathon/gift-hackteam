05f324785a1f0f02db4019f3487a00f545fc0994
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-10-22 11:37:31 -0400

    add different colours for different types of events

--- angular_seed/app/services/eventService.js
+++ angular_seed/app/services/eventService.js
@@ -19,13 +19,42 @@
                     var numEvents = eventService.monthlyEvents.length;
 		            eventService.monthlyEvents.splice(0,numEvents);
 				    for(var i = 0; i<data.length; i++){//add events to array one by one
+                        var colour = eventService.pickEventColour(data[i].type);
+                        data[i].color = colour;
                         eventService.monthlyEvents.push(data[i]);
                     }
+                    console.log(eventService.monthlyEvents);
                 },
                 function(err){
                     alert("error with getAllGroups");
                 });
 	    }
+
+        eventService.pickEventColour = function(type) {
+            if(type == "volunteer-meeting"){
+                return '#6371C6';
+            }
+            else if (type == "social"){
+                return '#37a3fb';
+            }
+            else if (type == "lunch"){
+                return '#2ca09e';
+            }
+            else if (type == "board-meeting"){
+                return '#990D6F';
+            }
+            else if (type == "family-meeting"){
+                return '#5931b4';
+            }
+            else if (type == "board-meeting"){
+                return '#5931b4';
+            }
+            else {
+                return '#2EB671';
+            }
+        }
+
+
         return eventService;
     }
 })();
