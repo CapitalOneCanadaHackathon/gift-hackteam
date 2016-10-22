849c6e2075abf220ad495ec5604a93b2576ea376
Vasuthan Vinayagamoorthy <vinaya.vasuthan@gmail.com>
2016-10-22 15:31:34 -0400

    server.js edits being made

--- angular_seed/server.js
+++ angular_seed/server.js
@@ -22,6 +22,19 @@ app.get('/basictest', function(req, res) {
     }
 });
 
+app.post('/test', function(req, res){
+
+    db.connect(done);
+//{title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29), eventID:6, type:"board-meeting",location:"123 address st.",description:"this is a great meeting"}
+    function done(){
+        var pool = db.get();
+        var events = [];
+        pool.query('SELECT * FROM eventslog', function(err, results){
+            events.push({"title": });
+        });
+    }
+});
+
 
 app.listen(8080);
 console.log('Magic happens on 8080');
