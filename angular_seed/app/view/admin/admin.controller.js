039df15fd2bb433dc9cbd78a6ca68638ca036308
Rebecca Song <rebecca.song@outlook.com>
2016-10-22 09:59:33 -0400

    Added angular-bootstrap and admin

--- angular_seed/app/view/admin/admin.controller.js
+++ angular_seed/app/view/admin/admin.controller.js
@@ -7,6 +7,34 @@
         vm.test = "ADMINNNN";
         //this should be displayed
         
-        
+        vm.users = [
+
+            {
+            "firstname": "Person",
+            "lastname": "One",
+            "email": "personone@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Two",
+            "email": "persontwo@email.com",
+            "isAdmin": true,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Three",
+            "email": "personthree@email.com",
+            "isAdmin": false,
+            },
+            {
+            "firstname": "Person",
+            "lastname": "Four",
+            "email": "personfour@email.com",
+            "isAdmin": true,
+            }
+        ];
+
+
     }
 })();
