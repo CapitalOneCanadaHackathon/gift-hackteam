7bbd34419485ffecaeaed3576ad2afd844354d05
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-10-23 00:02:34 -0400

    session confirmation, profile and user

--- angular_seed/app/view/users/users.controller.js
+++ angular_seed/app/view/users/users.controller.js
@@ -2,9 +2,9 @@
     'use strict';
     angular.module('faver.users').controller('UsersController', UsersController);
 
-    UsersController.$inject = ['$scope','UsersService'];
+    UsersController.$inject = ['$scope','UsersService','LoginService'];
 
-    function UsersController($scope, UsersService){
+    function UsersController($scope, UsersService,LoginService){
     	
     	$scope.firstNameList = [];
     	$scope.lastNameList = [];
@@ -12,6 +12,10 @@
 
     	UsersService.getUsersList();//generate users array through API serivce 
         $scope.usersList = UsersService.usersList; // ?
+
+         //confirm the user has logged in on page load
+        //TODO: should do this and confirm before making database calls
+        LoginService.confirmSession();
     }
 
 })();
